c055172fe8e3b35a8cea3ce31f0957ab57542abb
jj <a@b.c>
2024-05-12 17:31:54 -0400

    more printings

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,15 @@ import * as process from 'process'
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    core.error(JSON.stringify(process.env))
-
-    const imgToRepoArn = core.getInput('imgToRepoArn', {
-      required: true,
-      trimWhitespace: true
-    })
-    const argoManifestRepoArn = core.getInput('argoManifestRepoArn', {
-      required: true,
-      trimWhitespace: true
-    })
-    const clusterEndpoint = core.getInput('clusterEndpoint', {
-      required: true,
-      trimWhitespace: true
-    })
-
-    core.error(`imgToRepoArn: ${imgToRepoArn}`)
-    core.error(`argoManifestRepoArn: ${argoManifestRepoArn}`)
-    core.error(`clusterEndpoint: ${clusterEndpoint}`)
+    for (const tmp in process.env) {
+      core.error(tmp + '>>>>' + process.env[tmp])
+    }
 
     const sts = new STSClient({ region: process.env.AWS_DEFAULT_REGION })
     const callerId = await sts.send(new GetCallerIdentityCommand({}))
+
     core.error('>>>>')
     core.error(JSON.stringify(callerId, null, 2))
     core.error('<<<<')
-  } catch (error) {
-    core.error(JSON.stringify(error as Error, null, 2))
-  }
+
 }
